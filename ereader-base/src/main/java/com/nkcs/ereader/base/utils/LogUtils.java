5da6d7d3a0494bd4acc7e1ad5f6ddf0ab58fd92d
faunleaf <faunleaf@aliyun.com>
2018-04-01 21:58:17 +0800

    add log utils (#4)

---
+++ ereader-base/src/main/java/com/nkcs/ereader/base/utils/LogUtils.java
@@ -0,0 +1,255 @@
+package com.nkcs.ereader.base.utils;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import com.nkcs.ereader.base.BaseApplication;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author faunleaf
+ * @date 2018/4/1
+ */
+
+public class LogUtils {
+
+    /**
+     * 日志文件总开关
+     */
+    private static Boolean LOG_SWITCH = true;
+    /**
+     * 日志写入文件开关
+     */
+    private static Boolean LOG_TO_FILE = false;
+    /**
+     * 默认的tag
+     */
+    private static String LOG_TAG = "EReader";
+    /**
+     * 输入日志类型，v代表输出所有信息,w则只输出警告...
+     */
+    private static char LOG_TYPE = 'v';
+    /**
+     * sd卡中日志文件的最多保存天数
+     */
+    private static int LOG_SAVE_DAYS = 7;
+
+    /**
+     * 日志的输出格式
+     */
+    private final static SimpleDateFormat LOG_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    /**
+     * 日志文件格式
+     */
+    private final static SimpleDateFormat FILE_SUFFIX = new SimpleDateFormat("yyyy-MM-dd");
+    /**
+     * 日志文件保存路径
+     */
+    private static String LOG_FILE_PATH;
+    /**
+     * 日志文件保存名称
+     */
+    private static String LOG_FILE_NAME;
+
+    public static void init(Context context) {
+        LOG_FILE_PATH = Environment.getExternalStorageDirectory().getPath() + File.separator
+                + BaseApplication.getContext().getPackageName();
+        LOG_FILE_NAME = "Log";
+    }
+
+    /****************************
+     * Warn
+     *********************************/
+    public static void w(Object msg) {
+        w(LOG_TAG, msg);
+    }
+
+    public static void w(String tag, Object msg) {
+        w(tag, msg, null);
+    }
+
+    public static void w(String tag, Object msg, Throwable tr) {
+        if (msg == null) return;
+        log(tag, msg.toString(), tr, 'w');
+    }
+
+    /***************************
+     * Error
+     ********************************/
+    public static void e(Object msg) {
+        e(LOG_TAG, msg);
+    }
+
+    public static void e(String tag, Object msg) {
+        e(tag, msg, null);
+    }
+
+    public static void e(String tag, Object msg, Throwable tr) {
+        if (msg == null) return;
+        log(tag, msg.toString(), tr, 'e');
+    }
+
+    /***************************
+     * Debug
+     ********************************/
+    public static void d(Object msg) {
+        d(LOG_TAG, msg);
+    }
+
+    public static void d(String tag, Object msg) {// 调试信息
+        d(tag, msg, null);
+    }
+
+    public static void d(String tag, Object msg, Throwable tr) {
+        if (msg == null) return;
+        log(tag, msg.toString(), tr, 'd');
+    }
+
+    /****************************
+     * Info
+     *********************************/
+    public static void i(Object msg) {
+        i(LOG_TAG, msg);
+    }
+
+    public static void i(String tag, Object msg) {
+        i(tag, msg, null);
+    }
+
+    public static void i(String tag, Object msg, Throwable tr) {
+        if (msg == null) return;
+        log(tag, msg.toString(), tr, 'i');
+    }
+
+    /**************************
+     * Verbose
+     ********************************/
+    public static void v(Object msg) {
+        v(LOG_TAG, msg);
+    }
+
+    public static void v(String tag, Object msg) {
+        v(tag, msg, null);
+    }
+
+    public static void v(String tag, Object msg, Throwable tr) {
+        if (msg == null) return;
+        log(tag, msg.toString(), tr, 'v');
+    }
+
+    /**
+     * 根据tag, msg和等级，输出日志
+     *
+     * @param tag
+     * @param msg
+     * @param level
+     */
+    private static void log(String tag, String msg, Throwable tr, char level) {
+        if (tag == null || msg == null || tr == null) {
+            return;
+        }
+        if (LOG_SWITCH) {
+            if ('e' == level && ('e' == LOG_TYPE || 'v' == LOG_TYPE)) {
+                Log.e(tag, createMessage(msg), tr);
+            } else if ('w' == level && ('w' == LOG_TYPE || 'v' == LOG_TYPE)) {
+                Log.w(tag, createMessage(msg), tr);
+            } else if ('d' == level && ('d' == LOG_TYPE || 'v' == LOG_TYPE)) {
+                Log.d(tag, createMessage(msg), tr);
+            } else if ('i' == level && ('d' == LOG_TYPE || 'v' == LOG_TYPE)) {
+                Log.i(tag, createMessage(msg), tr);
+            } else {
+                Log.v(tag, createMessage(msg), tr);
+            }
+            if (LOG_TO_FILE) {
+                log2File(String.valueOf(level), tag, msg + tr == null ? "" : "\n" + Log.getStackTraceString(tr));
+            }
+        }
+    }
+
+    private static String getFunctionName() {
+        StackTraceElement[] sts = Thread.currentThread().getStackTrace();
+        if (sts == null) {
+            return null;
+        }
+        for (StackTraceElement st : sts) {
+            if (st.isNativeMethod()) {
+                continue;
+            }
+            if (st.getClassName().equals(Thread.class.getName())) {
+                continue;
+            }
+            if (st.getFileName().equals("LogUtils.java")) {
+                continue;
+            }
+            return "[" + Thread.currentThread().getName() + "("
+                    + Thread.currentThread().getId() + "): " + st.getFileName()
+                    + ":" + st.getLineNumber() + "]";
+        }
+        return null;
+    }
+
+    private static String createMessage(String msg) {
+        String functionName = getFunctionName();
+        String message = (functionName == null ? msg
+                : (functionName + " - " + msg));
+        return message;
+    }
+
+    /**
+     * 打开日志文件并写入日志
+     *
+     * @return
+     **/
+    private synchronized static void log2File(String logType, String tag, String text) {
+        Date nowTime = new Date();
+        String date = FILE_SUFFIX.format(nowTime);
+        String dateLogContent = LOG_FORMAT.format(nowTime) + ":" + logType + ":" + tag + ":" + text;
+        File destDir = new File(LOG_FILE_PATH);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
+        File file = new File(LOG_FILE_PATH, LOG_FILE_NAME + date);
+        try {
+            FileWriter filerWriter = new FileWriter(file, true);
+            BufferedWriter bufWriter = new BufferedWriter(filerWriter);
+            bufWriter.write(dateLogContent);
+            bufWriter.newLine();
+            bufWriter.close();
+            filerWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除指定的日志文件
+     */
+    public static void delFile() {
+        String needDelFile = FILE_SUFFIX.format(getDateBefore());
+        File file = new File(LOG_FILE_PATH, needDelFile + LOG_FILE_NAME);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    /**
+     * 得到LOG_SAVE_DAYS天前的日期
+     *
+     * @return
+     */
+    private static Date getDateBefore() {
+        Date nowtime = new Date();
+        Calendar now = Calendar.getInstance();
+        now.setTime(nowtime);
+        now.set(Calendar.DATE, now.get(Calendar.DATE) - LOG_SAVE_DAYS);
+        return now.getTime();
+    }
+}
