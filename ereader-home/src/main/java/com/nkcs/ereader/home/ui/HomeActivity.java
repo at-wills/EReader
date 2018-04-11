9ecfb203a51e08df472d9a11fb2cf4fd42da724c
leto <1907984744@qq.com>
2018-04-12 03:43:02 +0800

    顶栏

---
+++ ereader-home/src/main/java/com/nkcs/ereader/home/ui/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.nkcs.ereader.home.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nkcs.ereader.home.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
